c652aa26d78c4ce0dce8874f1d658b387edf103c
maligujjar2801 <mhamza19851@gmail.com>
2026-07-15 13:54:38 -0700

    I've changed the input type from integer to float.

--- Day-01/temperature.py
+++ Day-01/temperature.py
@@ -1,7 +1,7 @@
 def temp_converter():
-    C=int(input("Enter the temprature in Celcius scale:"))
+    C=float(input("Enter the temprature in Celcius scale:"))
     F= (C*9/5) + 32
-    return f"The temprature in Farenheit scale is {F}."
+    return f"The temprature in Farenheit scale is {F:.2f}."
 print(temp_converter())
 
 
